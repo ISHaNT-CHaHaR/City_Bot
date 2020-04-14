4059aef9433ca07516dac309a14bd4a2792e974f
ISHaNT-CHaHaR <56401920+ISHaNT-CHaHaR@users.noreply.github.com>
2020-04-14 15:30:42 +0530

    completed city_bot
    .

--- bot.js
+++ bot.js
@@ -9,6 +9,18 @@ const bot = new Telegraf('1127895907:AAGugeyIqWLiO_nbMxBfAqmJrlOXEugrvJw');
 //    //    bot.telegram.sendPhoto(ctx.chat.id, 'this is stored as an array in ctx.message.photo ');
 // });
 
+bot.command(['start', 'help'], (ctx) => {
+   const message = `
+   HElP REFERENCE.
+   /newyork - get image 
+   /dubai - get GIF
+   /singapore - location 
+   /cities - get album
+   /citiesList - get list
+   `;
+   ctx.reply(message);
+});
+
 bot.command('newyork', (ctx) => {
    bot.telegram.sendChatAction(ctx.chat.id, 'upload_photo');
 
@@ -53,4 +65,39 @@ bot.command('cities', (ctx) => {
 
    bot.telegram.sendMediaGroup(ctx.chat.id, result);
 });
+
+bot.command('citiesList', (ctx) => {
+   bot.telegram.sendDocument(
+      ctx.chat.id,
+      {
+         source: 'res/citieslist.txt',
+      },
+      {
+         thumb: { source: 'res/dubai.jpg' },
+      }
+   );
+});
+
+bot.command('singapore', (ctx) => {
+   bot.telegram.sendPhoto(ctx.chat.id, {
+      source: 'res/singapore.jpg',
+   });
+   let lat = 1.3521;
+   let long = 103.8198;
+   bot.telegram.sendLocation(ctx.chat.id, lat, long);
+});
+
+bot.on('message', async (ctx) => {
+   try {
+      if (ctx.updateSubTypes[0] === 'document') {
+         let link = await bot.telegram.getFileLink(
+            ctx.message.document.file_id
+         );
+         ctx.reply('your download link: ' + link);
+      }
+   } catch (err) {
+      ctx.reply(err.description);
+   }
+});
+
 bot.launch();
